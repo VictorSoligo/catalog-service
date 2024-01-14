419de1902686dc5cd68d82536ac703ec32dd90e8
Victor Hugo Soligo <victor@pop-os.localdomain>
2024-01-14 20:23:13 -0300

    chore: env variables initial setup

---
+++ src/infra/env/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  PORT: z.number().default(3333),
+  MYSQL_USER: z.string(),
+  MYSQL_PASSWORD: z.string(),
+  MYSQL_DATABASE: z.string(),
+})
+
+export type Env = z.infer<typeof envSchema>
