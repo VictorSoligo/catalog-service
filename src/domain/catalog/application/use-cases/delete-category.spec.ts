9fd7c49f0ecc33c630a776d278a48db9b941be79
Victor Hugo Soligo <victor@pop-os.localdomain>
2024-01-28 12:16:50 -0300

    chore: test deleting a category that does not exist and deleting a category from another owner

--- src/domain/catalog/application/use-cases/delete-category.spec.ts
+++ src/domain/catalog/application/use-cases/delete-category.spec.ts
@@ -2,6 +2,8 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeCategory } from 'test/factories/make-category'
 import { InMemoryCategoriesRepository } from 'test/repositories/in-memory-categories-repository'
 import { DeleteCategoryUseCase } from './delete-category'
+import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let inMemoryCategoriesRepository: InMemoryCategoriesRepository
 let sut: DeleteCategoryUseCase
@@ -27,5 +29,32 @@ describe('delete category', () => {
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryCategoriesRepository.items).toHaveLength(0)
+  })
+
+  it('should be able to delete a category from another owner', async () => {
+    const category = makeCategory({
+      ownerId: new UniqueEntityID('owner-01'),
+    })
+
+    await inMemoryCategoriesRepository.create(category)
+
+    const result = await sut.execute({
+      ownerId: 'owner-33',
+      categoryId: category.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(inMemoryCategoriesRepository.items).toHaveLength(1)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+
+  it('should be able to delete a category that does not exist', async () => {
+    const result = await sut.execute({
+      ownerId: 'owner-01',
+      categoryId: 'category-01',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
